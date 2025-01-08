6d21ac76289c1739871778cf33678c7cdbb9f4d4
zaki709 <t0709zaki@gmail.com>
2025-01-08 15:18:30 +0900

    temp save

---
+++ src/resnet_model.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+from torchvision.models import resnet34
+
+
+class AudioClassifier(nn.Module):
+    def __init__(self):
+        super(AudioClassifier, self).__init__()
+        self.resnet = resnet34(pretrained=True)
+        self.resnet.conv1 = nn.Conv2d(
+            1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
+        )
+        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 2)
+
+    def uname(self):
+        return "resnet34 pretrained=True"
+
+    def forward(self, x):
+        return self.resnet(x)
